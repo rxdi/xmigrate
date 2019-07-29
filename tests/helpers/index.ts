21ecf11d940a0eded99a90a32e42859c49ffed0c
Kristiyan Tachev <kristiqn.tachev@gmail.com>
2019-07-29 18:06:32 +0300

    license(): init license applied

---
+++ tests/helpers/index.ts
@@ -0,0 +1,13 @@
+/**
+ * @license
+ * Copyright (c) 2019, Reactive Solutions LTD 205455032. All rights reserved.
+ * This code may only be used under the BSD style license found at
+ * https://github.com/rxdi/xmigrate/blob/master/LICENSE
+ * The complete set of authors may be found at
+ * https://github.com/rxdi/xmigrate/blob/master/AUTHORS
+ * Code distributed by Reactive Solutions as part of the `@rxdi` project is also
+ * subject to an additional IP rights grant found at
+ * https://github.com/rxdi/xmigrate/blob/master/PATENTS
+ */
+
+export * from './fake-mongo';
