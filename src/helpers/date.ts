4d3ee8e37f6902a0ef5e62b06c4fe9b1d298e759
Kristiyan Tachev <kristiqn.tachev@gmail.com>
2020-10-22 11:34:17 +0300

    fix(Date): added prefix zero if seconds are less than 10

--- src/helpers/date.ts
+++ src/helpers/date.ts
@@ -6,14 +6,14 @@ export const formatDate = (date: Date) => {
   const hours = '' + d.getUTCHours();
   const minutes = '' + d.getUTCMinutes();
   const seconds = '' + d.getUTCSeconds();
-
+  const convertedSeconds = Number(seconds) < 10 ? [0, seconds].join('') : seconds;
   if (month.length < 2) {
     month = '0' + month;
   }
   if (day.length < 2) {
     day = '0' + day;
   }
-  return [year, month, day, hours, minutes, seconds].join('');
+  return [year, month, day, hours, minutes, convertedSeconds].join('');
 };
 
 export const nowAsString = () => formatDate(new Date());
